dac1c68ec52e14e900c16cf7d822b8c3045615ed
Ajay Yenugula <ajay.yenugula@valuemomentum.com>
2025-11-18 12:38:31 +0530

    first commit

---
+++ src/main/java/com/vm/service/policysubmission/PolicysubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.vm.service.policysubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicysubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicysubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vm/service/policysubmission/PolicysubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.service.policysubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicysubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
